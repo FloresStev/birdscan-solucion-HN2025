1b73ce536ed83ad04bcb72e0b9cdd8c0c819edd8
Steven Flores <sfloresblanco@gmail.com>
2025-09-27 22:53:11 -0600

    Finalización de configuración del baackend para la sección de reservas naturales

--- birdscan-backend/src/naturalreserves/naturalreserves.controller.ts
+++ birdscan-backend/src/naturalreserves/naturalreserves.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, BadRequestException } from '@nestjs/common';
 import { NaturalreservesService } from './naturalreserves.service';
 import { CreateNaturalreserveDto } from './dto/create-naturalreserve.dto';
 import { UpdateNaturalreserveDto } from './dto/update-naturalreserve.dto';
@@ -7,19 +7,80 @@ import { UpdateNaturalreserveDto } from './dto/update-naturalreserve.dto';
 export class NaturalreservesController {
   constructor(private readonly naturalreservesService: NaturalreservesService) {}
 
-  @Post()
-  create(@Body() createNaturalreserveDto: CreateNaturalreserveDto) {
-    return this.naturalreservesService.create(createNaturalreserveDto);
+  
+
+  @Get('all')
+  async findAll(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 20
+  ) {
+    const skip = (page - 1)* limit;
+    const reserves = await this.naturalreservesService.findMany({skip, take: limit});
+    const total = await this.naturalreservesService.count();
+
+    return {
+      message: 'Reservas obtenidas con éxito',
+      data: reserves,
+      total,
+      page,
+      totalPages: Math.ceil(total / limit),
+    };
   }
 
-  @Get()
-  findAll() {
-    return this.naturalreservesService.findAll();
+  @Get('search')
+  async searchReserves(@Query('q') query: string){
+    if (!query) {
+      throw new BadRequestException('Se requiere un término de búsqueda');
+    }
+
+    const results = await this.naturalreservesService.searchByName(query);
+
+    return{
+      message: 'Reservas encontradas exitosamente',
+      data:  results,
+    }
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.naturalreservesService.findOne(+id);
+  @Get('by-type')
+  async getByType(
+    @Query('type') protected_area_type: string,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 20
+  ) {
+    const typeReserves = protected_area_type.trim();
+    const skip = (page - 1) * limit;
+
+    const[reserves, total] = await Promise.all([
+      this.naturalreservesService.getReservesByType(typeReserves, skip, limit),
+      this.naturalreservesService.countReservesByTOM(typeReserves),
+    ]);
+
+    return {
+      message: 'Reserves filtered by type',
+      data: reserves,
+      totalPages: Math.ceil(total/limit),
+    };
+  }
+
+  @Get('by-municipality')
+  async getReservesByMunicipality(
+    @Query('municipality') municipality: string,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 20
+  ){
+    const skip = (page - 1) * limit;
+    const municipalityName = municipality.trim();
+
+    const [reserves, total] = await Promise.all([
+      this.naturalreservesService.getReservesByMunicipality(municipality, skip, limit),
+      this.naturalreservesService.countReservesByTOM(municipalityName),
+    ]);
+
+    return {
+      message: 'Reserves filtered by municipality',
+      data: reserves,
+      totalPages: Math.ceil(total / limit),
+    };
   }
 
   @Patch(':id')
